1f7555a558b37ca1fb8240e8125de1efc9d384b8
Muneef <muneef97@gmail.com>
2019-12-26 15:36:25 +0800

    functions are made row column functions are made

--- sample.py
+++ sample.py
@@ -20,6 +20,15 @@ def write_output(file,table):
         f.write("\n")
     return table
 
+def remove_element(table,row_begin,row_end,col_begin,col_end,table_row,table_col,a):
+    for i in range(row_begin,row_end):
+        for j in range(col_begin,col_end):
+
+            d = int(table[table_row + i][table_col + j])
+            if (d in a):
+                a.remove(d)
+    return a
+
 def solve_sudo_ku(table):
     k=4
     while (k != 0):
@@ -49,34 +58,19 @@ def solve_sudo_ku(table):
                     if ((row + 1) % 3 == 1):
                         if ((col + 1) % 3 == 1):
 
-                            for i in range(3):
-                                for j in range(3):
-
-                                    d = int(table[row + i][col + j])
-                                    if (d in a):
-                                        a.remove(d)
+                            remove_element(table,0,3,0,3,row,col,a)
 
                         # if row % 3 = 1, then 3x3 boxes will be row , row+1, row+2
                         # if col % 3 = 2 , then 3x3 boxes will be, col-1, col, col+1
                         elif ((col + 1) % 3 == 2):
 
-                            for i in range(3):
-                                for j in range(-1, 2):
-
-                                    e = int(table[row + i][col + j])
-                                    if (e in a):
-                                        a.remove(e)
+                            remove_element(table, 0, 3, -1, 2, row, col, a)
 
                         # if row % 3 = 1, then 3x3 boxes will be row , row+1, row+2
                         # if col % 3 = 0 , then 3x3 boxes will be, col-2, col-1, col
                         elif ((col + 1) % 3 == 0):
                             # print("")
-                            for i in range(3):
-                                for j in range(-2, 1):
-
-                                    e = int(table[row + i][col + j])
-                                    if (e in a):
-                                        a.remove(e)
+                            remove_element(table, 0, 3, -2, 1, row, col, a)
 
                     # if row % 3 = 2, then 3x3 boxes will be row-1 , row, row+1
                     # if col % 3 = 1 , then 3x3 boxes will be, col, col+1, col+2
@@ -84,74 +78,38 @@ def solve_sudo_ku(table):
 
                         if ((col + 1) % 3 == 1):
 
-                            for i in range(-1, 2):
-
-                                for j in range(3):
-
-                                    e = int(table[row + i][col + j])
-                                    if (e in a):
-                                        a.remove(e)
-
-
+                            remove_element(table, -1, 2, 0, 3, row, col, a)
 
                         # if row % 3 = 2, then 3x3 boxes will be row-1 , row, row+1
                         # if col % 3 = 2 , then 3x3 boxes will be, col-1, col, col+1
                         elif ((col + 1) % 3 == 2):
 
-                            for i in range(-1, 2):
-
-                                for j in range(-1, 2):
-
-                                    e = int(table[row + i][col + j])
-                                    if (e in a):
-                                        a.remove(e)
-
+                            remove_element(table, -1, 2, -1, 2, row, col, a)
                         # if row % 3 = 2, then 3x3 boxes will be row-1 , row, row+1
                         # if col % 3 = 0 , then 3x3 boxes will be, col-2, col-1, col
                         elif ((col + 1) % 3 == 0):
 
-                            for i in range(-1, 2):
-
-                                for j in range(-2, 1):
-
-                                    e = int(table[row + i][col + j])
-                                    if (e in a):
-                                        a.remove(e)
+                            remove_element(table, -1, 2, -2, 1, row, col, a)
 
                     # if row % 3 = 0, then 3x3 boxes will be row-2 , row-1, row
                     # if col % 3 = 1 , then 3x3 boxes will be, col, col+1, col+2
                     if ((row + 1) % 3 == 0):
                         if ((col + 1) % 3 == 1):
 
-                            for i in range(-2, 1):
-                                for j in range(3):
-
-                                    e = int(table[row + i][col + j])
-                                    if (e in a):
-                                        a.remove(e)
-
+                            remove_element(table, -2, 1, 0, 3, row, col, a)
 
                         # if row % 3 = 0, then 3x3 boxes will be row-2 , row-1, row
                         # if col % 3 = 2 , then 3x3 boxes will be, col-1, col, col+1
                         elif ((col + 1) % 3 == 2):
 
-                            for i in range(-2, 1):
-                                for j in range(-1, 2):
-
-                                    e = int(table[row + i][col + j])
-                                    if (e in a):
-                                        a.remove(e)
+                            remove_element(table, -2, 1, -1, 2, row, col, a)
 
                         # if row % 3 = 0, then 3x3 boxes will be row-2 , row-1, row
                         # if col % 3 = 0 , then 3x3 boxes will be, col-2, col-1, col
                         elif ((col + 1) % 3 == 0):
 
-                            for i in range(-2, 1):
-                                for j in range(-2, 1):
+                            remove_element(table, -2, 1, -2, 1, row, col, a)
 
-                                    e = int(table[row + i][col + j])
-                                    if (e in a):
-                                        a.remove(e)
                 # if the array a contains only 1 element then that element will be printed in the corresponding table
                 if (len(a) == 1):
                     table[row][col] = a[0]
